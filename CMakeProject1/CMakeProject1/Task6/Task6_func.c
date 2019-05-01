2144940a0029f9e449413be3f319691d86d0aefe
SangrealSoave <48495284+SangrealSoave@users.noreply.github.com>
2019-05-01 04:10:05 +0300

    Add files via upload

---
+++ CMakeProject1/CMakeProject1/Task6/Task6_func.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+double result, x;
+void f()
+{
+	result = (sin(2 * x) + sin(5 * x) - sin(2 * x)) / (cos(x) - cos(3 * x) + cos(5 * x));
+}
---
+++ CMakeProject1/CMakeProject1/Task7/Task7_func.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include "func.h"
+double x, result;
+double result, x;
+void f()
+{
+	result = (sin(2 * x) + sin(5 * x) - sin(2 * x)) / (cos(x) - cos(3 * x) + cos(5 * x));
+}
